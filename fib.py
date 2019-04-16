2cb55c066f88373df3f279e3427c2a05775462d1
skay1899 <skay1899@users.noreply.github.com>
2019-04-16 23:57:12 +0530

    Update fib.py

--- fib.py
+++ fib.py
@@ -8,11 +8,11 @@ def fib(n):
 	if n <= 1:  
         return n  
     else:  
-        if FibArray[n - 1] == 0:  
-            FibArray[n - 1] = fibonacci(n - 1)  
+        if ar[n - 1] == 0:  
+            ar[n - 1] = fibonacci(n - 1)  
   
-        if FibArray[n - 2] == 0:  
-            FibArray[n - 2] = fibonacci(n - 2)  
+        if ar[n - 2] == 0:  
+            ar[n - 2] = fibonacci(n - 2)  
               
-    FibArray[n] = FibArray[n - 2] + FibArray[n - 1]  
-    return FibArray[n]
+    ar[n] = ar[n - 2] + ar[n - 1]  
+    return ar[n]
