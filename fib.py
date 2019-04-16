5d70b1e2b82518fc0c7e20711fa78fee2b2e49fc
skay1899 <skay1899@users.noreply.github.com>
2019-04-17 00:02:22 +0530

    Update fib.py

--- fib.py
+++ fib.py
@@ -5,14 +5,12 @@ def fib(n):
 	ar = [0,1]
 	while len(ar) < n+1:
 		ar.append(0)
-	if n <= 1:  
-        return n
-    else:  
-        if ar[n - 1] == 0:  
-            ar[n - 1] = fibonacci(n - 1)  
-  
-        if ar[n - 2] == 0:  
-            ar[n - 2] = fibonacci(n - 2)  
-              
-    ar[n] = ar[n - 2] + ar[n - 1]  
-    return ar[n]
+	if n <= 1:
+		return n
+	else:
+		if ar[n - 1] == 0:
+			ar[n - 1] = fibonacci(n - 1)
+		if ar[n - 2] == 0:
+			ar[n - 2] = fibonacci(n - 2)  
+	ar[n] = ar[n - 2] + ar[n - 1]
+	return ar[n]
